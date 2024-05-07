6c4d0f85621d0ddc11ecb3609e661dc8a27c0f2c
Louis <louisjoecodes@gmail.com>
2024-05-07 15:37:44 +0100

    fix: long errors overflowing

--- js/packages/quary-extension-ui/src/components/Warning.stories.tsx
+++ js/packages/quary-extension-ui/src/components/Warning.stories.tsx
@@ -16,3 +16,14 @@ export const Main: Story = {
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam quo totam eius aperiam dolorum.',
   },
 }
+
+export const LongError: Story = {
+  args: {
+    title: 'Attention needed',
+    children:
+    `Error reading .schema file: Failed to await js function: JsValue(EntryNotFound (FileSystemError): Error: ENOENT: no such file or directory, open '/Users/louisjordan/Desktop/analysis/quary_jaffle_shop/models/staging/schema.yaml'
+    EntryNotFound (FileSystemError): Error: ENOENT: no such file or directory, open '/Users/louisjordan/Desktop/analysis/quary_jaffle_shop/models/staging/schema.yaml'
+        at Function.e (/Applications/Visual Studio Code.app/Contents/Resources/app/out/vs/workbench/api/node/extensionHostProcess.js:149:6084)
+        at Object.readFile (/Applications/Visual Studio Code.app/Contents/Resources/app/out/vs/workbench/api/node/extensionHostProcess.js:149:4463))`,
+  },
+}
