8b92f36369fd965edb3060942c8439501c8262c5
Louis <louisjoecodes@gmail.com>
2024-05-13 12:03:27 +0100

    chore: add another failing test to reproduce error

--- rust/cli/tests/cli_test.rs
+++ rust/cli/tests/cli_test.rs
@@ -663,10 +663,7 @@ async fn test_postgres_run_tests_from_sources() {
     fs::create_dir_all(&models_dir).unwrap();
     let model_1_file = models_dir.join("model_1.sql");
     let model_1_content = "SELECT 100 as random_number";
-    let model_2_file = models_dir.join("model_2.sql");
-    let model_2_content = "SELECT * FROM q.model_1";
     fs::write(&model_1_file, model_1_content).unwrap();
-    fs::write(&model_2_file, model_2_content).unwrap();
 
     // Create quary.yaml file
     let quary_yaml_content = "postgres:\n  schema: public";
@@ -678,7 +675,6 @@ async fn test_postgres_run_tests_from_sources() {
     let schema_content = r#"
         models:
         - name: model_1
-        - name: model_2
           columns:
           - name: random_number
             description: "This is a random number which should never be null"
@@ -692,6 +688,70 @@ async fn test_postgres_run_tests_from_sources() {
             .unwrap()
             .current_dir(project_dir)
             .args(vec!["test", "-s"])
+            .assert()
+            .success();
+    }
+}
+
+
+/// This test simulates a workflow where a test is added to a model and we run quary test (from database).
+#[tokio::test]
+async fn test_postgres_run_tests_from_database_tables() {
+    // Setup
+    let postgres = RunnableImage::from(TestcontainersPostgres::default())
+        .start()
+        .await;
+
+    let name = "quary";
+    let temp_dir = tempdir().unwrap();
+    let project_dir = temp_dir.path();
+
+    // create a .env file
+    let env_file_path = project_dir.join(".env");
+    let env_content = format!(
+        "PGHOST=localhost\nPGPORT={}\nPGUSER=postgres\nPGPASSWORD=postgres\nPGDATABASE=postgres",
+        postgres.get_host_port_ipv4(5432).await
+    );
+    fs::write(&env_file_path, env_content).unwrap();
+
+    // Create a model which reference a model
+    let models_dir: std::path::PathBuf = project_dir.join("models");
+    fs::create_dir_all(&models_dir).unwrap();
+    let model_1_file = models_dir.join("model_1.sql");
+    let model_1_content = "SELECT 100 as random_number";
+    fs::write(&model_1_file, model_1_content).unwrap();
+
+    // Create quary.yaml file
+    let quary_yaml_content = "postgres:\n  schema: public";
+    let quary_yaml_path = project_dir.join("quary.yaml");
+    fs::write(&quary_yaml_path, quary_yaml_content).unwrap();
+
+    // Create schema.yaml file
+    let schema_file = models_dir.join("schema.yaml");
+    let schema_content = r#"
+        models:
+        - name: model_1
+          columns:
+          - name: random_number
+            description: "This is a random number which should never be null"
+            tests:
+            - type: not_null
+            - type: unique
+        "#;
+    fs::write(&schema_file, schema_content).unwrap();
+    {
+        Command::cargo_bin(name)
+            .unwrap()
+            .current_dir(project_dir)
+            .args(vec!["build"])
+            .assert()
+            .success();
+    }
+    {
+        Command::cargo_bin(name)
+            .unwrap()
+            .current_dir(project_dir)
+            .args(vec!["test"])
             .assert()
             .success();
     }
