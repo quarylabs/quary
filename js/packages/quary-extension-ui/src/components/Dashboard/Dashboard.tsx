ec0d599395b42a090ecdc9732723f4852179ba8d
Ben King <9087625+benfdking@users.noreply.github.com>
2024-06-24 15:19:46 +0100

    temp

--- js/packages/quary-extension-ui/src/components/Dashboard/Dashboard.tsx
+++ js/packages/quary-extension-ui/src/components/Dashboard/Dashboard.tsx
@@ -1,158 +1,160 @@
-import * as React from "react";
-import _ from "lodash";
-import Responsive from '../../lib/ResponsiveReactGridLayout';
-import WidthProvider from '../../lib/components/WidthProvider';
-import type {CompactType, Layout, LayoutItem, ReactChildren} from '../../lib/utils';
-import type {Breakpoint, OnLayoutChangeCallback} from '../../lib/responsiveUtils';
-const ResponsiveReactGridLayout = WidthProvider(Responsive);
+import { Layout, Responsive, WidthProvider } from 'react-grid-layout'
+import _ from 'lodash'
+import { EventHandler, Component, SyntheticEvent } from 'react'
 
-type Props = {|
-    className: string,
-    cols: {[string]: number},
-    onLayoutChange: Function,
-    rowHeight: number,
-    |};
-type State = {|
-    currentBreakpoint: string,
-    compactType: CompactType,
-    mounted: boolean,
-    resizeHandles: string[],
-    layouts: {[string]: Layout}
-        |};
+interface Props {
+  className: string
+  cols: Record<string, number>
+  onLayoutChange: Function
+  rowHeight: number
+}
 
-const availableHandles = ["s", "w", "e", "n", "sw", "nw", "se", "ne"];
+interface State {
+  currentBreakpoint: string
+  compactType: CompactType
+  mounted: boolean
+  resizeHandles: string[]
+  layouts: Record<string, Layout>
+}
 
-export default class ShowcaseLayout extends React.Component<Props, State> {
-    static defaultProps: Props = {
-        className: "layout",
-        rowHeight: 30,
-        onLayoutChange: function() {},
-        cols: { lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 },
-    };
+export type CompactType = 'horizontal' | 'vertical'
 
-    state: State = {
-        currentBreakpoint: "lg",
-        compactType: "vertical",
-        resizeHandles: ['se'],
-        mounted: false,
-        layouts: { lg: generateLayout(['se']) }
-    };
+const availableHandles = ['s', 'w', 'e', 'n', 'sw', 'nw', 'se', 'ne']
 
-    componentDidMount() {
-        this.setState({ mounted: true });
-    }
+const ResponsiveReactGridLayout = WidthProvider(Responsive)
 
-    generateDOM(): ReactChildren {
-        return _.map(this.state.layouts.lg, function(l, i) {
-            return (
-                <div key={i} className={l.static ? "static" : ""}>
-                    {l.static ? (
-                        <span
-                            className="text"
-                            title="This item is static and cannot be removed or resized."
-                        >
+export default class Dashboard extends Component<Props, State> {
+  static defaultProps: Props = {
+    className: 'layout',
+    rowHeight: 30,
+    onLayoutChange() {},
+    cols: { lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 },
+  }
+
+  state: State = {
+    currentBreakpoint: 'lg',
+    compactType: 'vertical',
+    resizeHandles: ['se'],
+    mounted: false,
+    layouts: { lg: generateLayout(['se']) },
+  }
+
+  componentDidMount() {
+    this.setState({ mounted: true })
+  }
+
+  generateDOM(): React.ReactNodeArray {
+    return _.map(this.state.layouts.lg, function (l, i) {
+      return (
+        <div key={i} className={l.static ? 'static' : ''}>
+          {l.static ? (
+            <span
+              className="text"
+              title="This item is static and cannot be removed or resized."
+            >
               Static - {i}
             </span>
-                    ) : (
-                        <span className="text">{i}</span>
-                    )}
-                </div>
-            );
-        });
-    }
+          ) : (
+            <span className="text">{i}</span>
+          )}
+        </div>
+      )
+    })
+  }
 
-    onBreakpointChange: (Breakpoint) => void = (breakpoint) => {
-        this.setState({
-            currentBreakpoint: breakpoint
-        });
-    };
+  onBreakpointChange: (Breakpoint) => void = (breakpoint) => {
+    this.setState({
+      currentBreakpoint: breakpoint,
+    })
+  }
 
-    onCompactTypeChange: () => void = () => {
-        const { compactType: oldCompactType } = this.state;
-        const compactType =
-            oldCompactType === "horizontal"
-                ? "vertical"
-                : oldCompactType === "vertical"
-                    ? null
-                    : "horizontal";
-        this.setState({ compactType });
-    };
+  onCompactTypeChange: () => void = () => {
+    const { compactType: oldCompactType } = this.state
+    const compactType =
+      oldCompactType === 'horizontal'
+        ? 'vertical'
+        : oldCompactType === 'vertical'
+          ? null
+          : 'horizontal'
+    this.setState({ compactType })
+  }
 
-    onResizeTypeChange: () => void = () => {
-        const resizeHandles = this.state.resizeHandles === availableHandles ? ['se'] : availableHandles;
-        this.setState({resizeHandles, layouts: {lg: generateLayout(resizeHandles)}});
-    };
+  onResizeTypeChange: () => void = () => {
+    const resizeHandles =
+      this.state.resizeHandles === availableHandles ? ['se'] : availableHandles
+    this.setState({
+      resizeHandles,
+      layouts: { lg: generateLayout(resizeHandles) },
+    })
+  }
 
+  onLayoutChange = (layout, layouts) => {
+    this.props.onLayoutChange(layout, layouts)
+  }
 
-    onLayoutChange: OnLayoutChangeCallback = (layout, layouts) => {
-        this.props.onLayoutChange(layout, layouts);
-    };
+  onNewLayout: EventHandler<SyntheticEvent> = () => {
+    this.setState({
+      layouts: { lg: generateLayout(this.state.resizeHandles) },
+    })
+  }
 
-    onNewLayout: EventHandler = () => {
-        this.setState({
-            layouts: { lg: generateLayout(this.state.resizeHandles) }
-        });
-    };
+  onDrop: (layout: Layout[], item: Layout, e: Event) => void = (elemParams) => {
+    alert(`Element parameters: ${JSON.stringify(elemParams)}`)
+  }
 
-    onDrop: (layout: Layout, item: ?LayoutItem, e: Event) => void = (elemParams) => {
-        alert(`Element parameters: ${JSON.stringify(elemParams)}`);
-    };
-
-    render(): React.Node {
-        // eslint-disable-next-line no-unused-vars
-        return (
-            <div>
-                <div>
-                    Current Breakpoint: {this.state.currentBreakpoint} (
-                    {this.props.cols[this.state.currentBreakpoint]} columns)
-                </div>
-                <div>
-                    Compaction type:{" "}
-                    {_.capitalize(this.state.compactType) || "No Compaction"}
-                </div>
-                <button onClick={this.onNewLayout}>Generate New Layout</button>
-                <button onClick={this.onCompactTypeChange}>
-                    Change Compaction Type
-                </button>
-                <button onClick={this.onResizeTypeChange}>
-                    Resize {this.state.resizeHandles === availableHandles ? "One Corner" : "All Corners"}
-                </button>
-                <ResponsiveReactGridLayout
-                    {...this.props}
-                    layouts={this.state.layouts}
-                    onBreakpointChange={this.onBreakpointChange}
-                    onLayoutChange={this.onLayoutChange}
-                    onDrop={this.onDrop}
-                    // WidthProvider option
-                    measureBeforeMount={false}
-                    // I like to have it animate on mount. If you don't, delete `useCSSTransforms` (it's default `true`)
-                    // and set `measureBeforeMount={true}`.
-                    useCSSTransforms={this.state.mounted}
-                    compactType={this.state.compactType}
-                    preventCollision={!this.state.compactType}
-                >
-                    {this.generateDOM()}
-                </ResponsiveReactGridLayout>
-            </div>
-        );
-    }
+  render(): React.ReactNode {
+    // eslint-disable-next-line no-unused-vars
+    return (
+      <div>
+        <div>
+          Current Breakpoint: {this.state.currentBreakpoint} (
+          {this.props.cols[this.state.currentBreakpoint]} columns)
+        </div>
+        <div>
+          Compaction type:{' '}
+          {_.capitalize(this.state.compactType) || 'No Compaction'}
+        </div>
+        <button onClick={this.onNewLayout}>Generate New Layout</button>
+        <button onClick={this.onCompactTypeChange}>
+          Change Compaction Type
+        </button>
+        <button onClick={this.onResizeTypeChange}>
+          Resize{' '}
+          {this.state.resizeHandles === availableHandles
+            ? 'One Corner'
+            : 'All Corners'}
+        </button>
+        <ResponsiveReactGridLayout
+          {...this.props}
+          layouts={this.state.layouts}
+          onBreakpointChange={this.onBreakpointChange}
+          onLayoutChange={this.onLayoutChange}
+          onDrop={this.onDrop}
+          // WidthProvider option
+          measureBeforeMount={false}
+          // I like to have it animate on mount. If you don't, delete `useCSSTransforms` (it's default `true`)
+          // and set `measureBeforeMount={true}`.
+          useCSSTransforms={this.state.mounted}
+          compactType={this.state.compactType}
+          preventCollision={!this.state.compactType}
+          children={this.generateDOM()}
+        />
+      </div>
+    )
+  }
 }
 
 function generateLayout(resizeHandles) {
-    return _.map(_.range(0, 25), function(item, i) {
-        var y = Math.ceil(Math.random() * 4) + 1;
-        return {
-            x: Math.round(Math.random() * 5) * 2,
-            y: Math.floor(i / 6) * y,
-            w: 2,
-            h: y,
-            i: i.toString(),
-            static: Math.random() < 0.05,
-            resizeHandles
-        };
-    });
-}
-
-if (process.env.STATIC_EXAMPLES === true) {
-    import("../test-hook.jsx").then(fn => fn.default(ShowcaseLayout));
+  return _.map(_.range(0, 25), function (item, i) {
+      const y = Math.ceil(Math.random() * 4) + 1
+      return {
+        x: Math.round(Math.random() * 5) * 2,
+        y: Math.floor(i / 6) * y,
+        w: 2,
+        h: y,
+        i: i.toString(),
+        static: Math.random() < 0.05,
+        resizeHandles,
+      }
+  })
 }
