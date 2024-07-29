f3b5f379a83bcb4b881b63ccfc0a12ca64866cdb
Ben King <9087625+benfdking@users.noreply.github.com>
2024-07-29 16:39:20 +0100

    chore: centralise cargo settings and bump

--- .hacking/scripts/check_versions_match.sh
+++ .hacking/scripts/check_versions_match.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
 # Extract version from Cargo.toml
-cargo_version_cli=$(grep "^version" rust/cli/Cargo.toml | awk -F '"' '{print $2}')
-cargo_version_core=$(grep "^version" rust/core/Cargo.toml | awk -F '"' '{print $2}')
-cargo_version_databases=$(grep "^version" rust/quary-databases/Cargo.toml | awk -F '"' '{print $2}')
-cargo_version_dbt=$(grep "^version" rust/dbt-converter/Cargo.toml | awk -F '"' '{print $2}')
-cargo_version_sqlinference=$(grep "^version" rust/sqlinference/Cargo.toml | awk -F '"' '{print $2}')
-cargo_version_wasm=$(grep "^version" rust/wasm-binding/Cargo.toml | awk -F '"' '{print $2}')
+cargo_version=$(grep "^version" Cargo.toml | awk -F '"' '{print $2}')
 
 # Extract version from package.json
 ts_version=$(jq -r '.version' js/packages/quary-extension/package.json)
@@ -22,12 +17,7 @@ compare_versions() {
     fi
 }
 
-compare_versions "$cargo_version_cli" "$ts_version"
-compare_versions "$cargo_version_core" "$ts_version"
-compare_versions "$cargo_version_databases" "$ts_version"
-compare_versions "$cargo_version_dbt" "$ts_version"
-compare_versions "$cargo_version_sqlinference" "$ts_version"
-compare_versions "$cargo_version_wasm" "$ts_version"
+compare_versions "$cargo_version" "$ts_version"
 
 # If GitHub release version is provided, compare it as well
 if [ -n "$github_release_version" ]; then
